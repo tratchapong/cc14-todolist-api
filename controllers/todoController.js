dc147f5fb5c51de6598d3d5d8729f10657fb854d
tratchapong <tratchapong@gmail.com>
2023-05-22 07:09:03 +0700

    update todoController : updateTodo

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,27 +1,31 @@
-const {Todo, User, sequelize} = require('../models')
+const { Todo, User, sequelize } = require("../models");
 
 exports.getAllTodos = (req, res, next) => {
-    console.log(req.user)
-    const {id} = req.user
-    Todo.findAll({
-        where: {userId : id}
-    }).then(rs => {
-        res.json(rs)
-    }).catch(next)
-}
+  console.log(req.user);
+  const { id } = req.user;
+  Todo.findAll({
+    where: { userId: id },
+  })
+    .then((rs) => {
+      res.json(rs);
+    })
+    .catch(next);
+};
 
 // ต้องการเฉพาะ title, due_date, status
 exports.getTodoById = (req, res, next) => {
-    const {id} = req.params
-    Todo.findAll({
-        attributes: ['title', 'dueDate', 'status'],
-        where : {id : id}
-    }).then( rs => {
-        res.json(rs)
-    }).catch(next)
-}
+  const { id } = req.params;
+  Todo.findAll({
+    attributes: ["title", "dueDate", "status"],
+    where: { id: id },
+  })
+    .then((rs) => {
+      res.json(rs);
+    })
+    .catch(next);
+};
 
-// เพิ่มข้อมูลโดยส่งมาทาง req.body 
+// เพิ่มข้อมูลโดยส่งมาทาง req.body
 
 // {
 //     "title": "Learn HTML",
@@ -30,29 +34,32 @@ exports.getTodoById = (req, res, next) => {
 // }
 
 exports.createTodo = (req, res, next) => {
-    const {title, dueDate} = req.body
-    // validation
-    Todo.create({
-        title : title,
-        dueDate : dueDate,
-        userId : req.user.id
-    }).then(rs=> {
-        res.json(rs)
-    }).catch(next)
-
-}
+  const { title, dueDate } = req.body;
+  // validation
+  Todo.create({
+    title: title,
+    dueDate: dueDate,
+    userId: req.user.id,
+  })
+    .then((rs) => {
+      res.json(rs);
+    })
+    .catch(next);
+};
 
 exports.deleteTodo = (req, res, next) => {
-    const {id} = req.params
-    Todo.destroy({
-        where : {id : id}
-    }).then(rs=> {
-        if (rs===0) {
-            throw new Error('Cannot Delete!!')
-        }
-        res.json(rs)
-    }).catch(next)
-}
+  const { id } = req.params;
+  Todo.destroy({
+    where: { id: id },
+  })
+    .then((rs) => {
+      if (rs === 0) {
+        throw new Error("Cannot Delete!!");
+      }
+      res.json(rs);
+    })
+    .catch(next);
+};
 
 // {
 //     "title": "Learn HTML",
@@ -61,111 +68,121 @@ exports.deleteTodo = (req, res, next) => {
 // }
 
 exports.updateTodo = (req, res, next) => {
-    const {id} = req.params
-    Todo.update(req.body, {
-        where : { id:id}
-    }).then(rs => {
-        res.json(rs)
-    }).catch(next)
-}
+  const { id } = req.params;
+  const { title, dueDate, status } = req.body;
+  Todo.update(
+    { ...req.body, userId: req.user.id },
+    {
+      where: { id: id },
+    }
+  )
+    .then((rs) => {
+      res.json(rs);
+    })
+    .catch(next);
+};
 
 // แสดง todolist จากชื่อ user
 //   /user?name=Andy
 
 exports.getTodoByUser = (req, res, next) => {
-    const {name} = req.query
-    User.findAll({
-        attributes: {exclude: 'password'},
-        where : { name : name},
-        include : {
-            model : Todo,
-            attributes: ['title', 'dueDate', 'status', 'remainDay']
-        }
-    }).then(rs => {
-        res.json(rs)
-    }).catch(next)
-}
+  const { name } = req.query;
+  User.findAll({
+    attributes: { exclude: "password" },
+    where: { name: name },
+    include: {
+      model: Todo,
+      attributes: ["title", "dueDate", "status", "remainDay"],
+    },
+  })
+    .then((rs) => {
+      res.json(rs);
+    })
+    .catch(next);
+};
 
 exports.summaryTodo = (req, res, next) => {
-    let countAll  = Todo.count({
-        where : { userId : req.user.id}
+  let countAll = Todo.count({
+    where: { userId: req.user.id },
+  });
+  let countDone = Todo.count({
+    where: {
+      status: true,
+      userId: req.user.id,
+    },
+  });
+  let countUnDone = Todo.count({
+    where: {
+      status: false,
+      userId: req.user.id,
+    },
+  });
+  Promise.all([countAll, countDone, countUnDone])
+    .then((rs) => {
+      res.json({
+        all: rs[0],
+        done: rs[1],
+        unDone: rs[2],
+      });
     })
-    let countDone = Todo.count({
-        where : { 
-            status : true,
-            userId : req.user.id
-        }
-    })
-    let countUnDone = Todo.count({
-        where : {
-            status : false,
-            userId : req.user.id
-        }
-    })
-    Promise.all([ countAll, countDone, countUnDone])
-    .then(rs => {
-
-        res.json({
-            all : rs[0],
-            done : rs[1],
-            unDone : rs[2]
-        })
-    }).catch(next)
-}
+    .catch(next);
+};
 
 exports.doubleDelete = async (req, res, next) => {
-    const  {id1, id2} = req.params
+  const { id1, id2 } = req.params;
 
-    const t = await sequelize.transaction()
+  const t = await sequelize.transaction();
 
-    try {
-        let rs1 = await Todo.destroy({
-            where: {id: id1},
-            transaction : t
-        })
-            if (rs1===0)
-                throw new Error('Cannot delete 1st id')
-        let rs2 = await Todo.destroy({
-            where: {id: id2} ,
-            transaction : t
-        })
-            if (rs2===0)
-                throw new Error('Cannot delete 2nd id')
-        await t.commit()
-        res.json({msg: `delete id: ${id1}, ${id2}`})
-    }catch(err) {
-        await t.rollback()
-        console.log('rollBack...')
-        next(err)
-    } 
-}   
+  try {
+    let rs1 = await Todo.destroy({
+      where: { id: id1 },
+      transaction: t,
+    });
+    if (rs1 === 0) throw new Error("Cannot delete 1st id");
+    let rs2 = await Todo.destroy({
+      where: { id: id2 },
+      transaction: t,
+    });
+    if (rs2 === 0) throw new Error("Cannot delete 2nd id");
+    await t.commit();
+    res.json({ msg: `delete id: ${id1}, ${id2}` });
+  } catch (err) {
+    await t.rollback();
+    console.log("rollBack...");
+    next(err);
+  }
+};
 
 exports.doubleDelete2 = (req, res, next) => {
-    const {id1, id2} = req.params
-    let tsc
-    sequelize.transaction().then( t => {
-        tsc = t
-        return Todo.destroy({
-            where: { id: id1 },
-            transaction: tsc
-        })
-    }).then( rs=> {
-        if (rs===0)
-            throw new Error('cannot delete 1st id')
-        return Todo.destroy({
-            where: {id: id2},
-            transaction: tsc
-        })
-    }).then( rs=> {
-        if(rs === 0)
-            throw new Error('cannot delete 2nd id')
-        return tsc.commit()
-    }).then( ()=> {
-        res.json({msg: `delete id: ${id1}, ${id2}` })
-    }).catch(err => {
-        tsc.rollback().then( ()=> {
-            console.log('rollBack...')
-            next(err)
-        })
+  const { id1, id2 } = req.params;
+  let tsc;
+  sequelize
+    .transaction()
+    .then((t) => {
+      tsc = t;
+      return Todo.destroy({
+        where: { id: id1 },
+        transaction: tsc,
+      });
     })
-}
+    .then((rs) => {
+      if (rs === 0) throw new Error("cannot delete 1st id");
+      return Todo.destroy({
+        where: { id: id2 },
+        transaction: tsc,
+      });
+    })
+    .then((rs) => {
+      if (rs === 0) throw new Error("cannot delete 2nd id");
+      return tsc.commit();
+    })
+    .then(() => {
+      res.json({ msg: `delete id: ${id1}, ${id2}` });
+    })
+    .catch((err) => {
+      tsc.rollback().then(() => {
+        console.log("rollBack...");
+        next(err);
+      });
+    });
+};
